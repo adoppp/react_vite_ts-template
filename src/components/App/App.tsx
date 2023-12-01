224f290597fd49f38a09d602d2f60c55947e3570
Adop <chdn786od@gmail.com>
2023-12-01 02:05:19 +0100

    initial

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-
 const App = () => {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100vw',
+            height: '100vh',
+            fontSize: 32,
+            fontWeight: 700,
+            textAlign: 'center',
+        }}>
+            Daniil Cheban's {'<Vite + React + Ts />'} template
+        </div>
+    );
 }
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App'; 
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
